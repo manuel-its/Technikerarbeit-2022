d93cd95c5dab49d56a16cb0d5d513355a36ede69
manuel-its <manuel.kalischko@its-stuttgart.de>
2022-02-12 16:34:42 +0100

    ddd

--- shadowsocks/shadowsocks.sh
+++ shadowsocks/shadowsocks.sh
@@ -4,8 +4,10 @@ apt-get update && apt-get upgrade -y
 echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee /etc/apt/sources.list.d/debian-buster-backports.list
 apt-get update
 apt -t buster-backports install shadowsocks-libev wget -y
-wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/shadowsocks/config.json
-mv -f config.json /etc/shadowsocks-libev/config.json
+#wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/shadowsocks/config.json
+wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/shadowsocks/config.json?token=GHSAT0AAAAAABRB2ABQBKKL57QNYPYEGWWQYQRBW4A
+cp 
+mv -f config.json?token=GHSAT0AAAAAABRB2ABQBKKL57QNYPYEGWWQYQRBW4A /etc/shadowsocks-libev/config.json
 echo "Bitte Server Passwort für Shadowsocks eingegen: "
 read password
 sed '/password/ s/" "/"'$password'"/p' /etc/shadowsocks-libev/config.json
