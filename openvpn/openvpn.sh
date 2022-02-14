af38fad7fa4ac341c6fbcd623cc3ebad8e06f2c4
manuel-its <manuel.kalischko@its-stuttgart.de>
2022-02-14 09:37:36 +0100

    ghjlk

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -12,5 +12,24 @@ cd .. && mv vpnserver /usr/local
 cd /usr/local/vpnserver/
 chmod 600 * chmod 700 vpnserver 
 chmod 700 vpncmd
-wget 
-
+wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/vpnserver?token=GHSAT0AAAAAABRB2ABR3GCQKETBTA66LYE6YQTJYJA
+cp vpnserver?token=GHSAT0AAAAAABRB2ABR3GCQKETBTA66LYE6YQTJYJA /etc/init.d/vpnserver
+mkdir /var/lock/subsys 
+chmod 755 /etc/init.d/vpnserver && /etc/init.d/vpnserver start 
+update-rc.d vpnserver defaults 
+./vpncmd in /usr/local/vpnserver
+ServerPasswordSet 
+HubCreate myhub 
+Hub myhub 
+SecureNatEnable 
+echo "Benutzername eingeben"
+read username
+UserCreate $username 
+UserPasswordSet $username 
+IPsecEnable
+yes
+ServerCertRegenerate ip/domain
+ServerCertGet ~/cert.cer 
+OpenVpnEnable yes /PORTS:1194 
+OpenVpnMakeConfig ~/openvpn_config.zip 
+ufw allow 500/udp && ufw allow 4500/udp && ufw allow 1194/udp 
