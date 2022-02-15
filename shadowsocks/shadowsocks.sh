12fe949e556637510b8a8e3399b2701c0cf29e67
manuel-its <manuel.kalischko@its-stuttgart.de>
2022-02-15 12:46:38 +0100

    shajkf

--- shadowsocks/shadowsocks.sh
+++ shadowsocks/shadowsocks.sh
@@ -1,14 +1,52 @@
 ##!/bin/bash
-# Installiere Shadowsocks und Passwort festlegen
+#Shadowsocks
+echo "Eingabe Zahl:"
+echo "1 Shadowsocks Installieren"
+echo "2 User hinzufügen(Multiuser einrichten)"
+echo "3 Multiuser deaktivieren"
+echo "4 Shadowsocks entfernen"
+read auswahl
+
+case $auswahl in
+   1)   
 apt-get update && apt-get upgrade -y
 echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee /etc/apt/sources.list.d/debian-buster-backports.list
 apt-get update
 apt -t buster-backports install shadowsocks-libev wget -y
-#wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/shadowsocks/config.json
 wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/shadowsocks/config.json?token=GHSAT0AAAAAABRB2ABQBKKL57QNYPYEGWWQYQRBW4A
-cp 
 mv -f config.json?token=GHSAT0AAAAAABRB2ABQBKKL57QNYPYEGWWQYQRBW4A /etc/shadowsocks-libev/config.json
 echo "Bitte Server Passwort für Shadowsocks eingegen: "
 read password
+$password
 sed '/password/ s/" "/"'$password'"/p' /etc/shadowsocks-libev/config.json
 systemctl restart shadowsocks-libev
+   ;;
+   2)   
+echo "Wie viele User sollen erstellt werden?"
+read anzahluser
+for ((n=1;n<=anzahluser;n++))
+do
+wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/shadowsocks/config.json?token=GHSAT0AAAAAABRB2ABQJ4BU7HBRQSZ53OXUYQUW44A
+mv config.json?token=GHSAT0AAAAAABRB2ABQJ4BU7HBRQSZ53OXUYQUW44A /etc/shadowsocks-libev/user$n
+forpassword=$(openssl rand -base64 32)
+echo $forpassword
+sed '/password/ s/" "/"'$forpassword'"/p' /etc/shadowsocks-libev/config$n.json
+systemctl enable shadowsocks-libev-server@user$n.service –now
+done
+;;
+   3)   
+
+anzahl=$(find /etc/shadowsocks-libev/ -type f | wc -l)
+if $anzahl>=2
+then
+   berechnung=anzahl-1
+   for ((n=1;n<=berechnung;n++))
+   do
+   systemctl disable shadowsocks-libev-server@user$n.service –now
+   rm /etc/shadowsocks-libev/config$n.json
+   done
+fi
+
+   ;;		  
+esac
+
