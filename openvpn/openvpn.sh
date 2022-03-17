8d7269b89aa13068cd5ec84942044e8ed93e174b
manuel-its <98522752+manuel-its@users.noreply.github.com>
2022-03-17 21:15:04 +0100

    Update openvpn.sh

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -1,11 +1,12 @@
 ##!/bin/bash
-# link https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/openvpn.sh?token=GHSAT0AAAAAABRB2ABRMF2KUIZYI2PVKEFWYQTJ6DQ
-# http://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
-# Installiere VPN
+#https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/openvpn.sh?token=GHSAT0AAAAAABSKUF6OPLBHSVCDFVOMCHDEYR42EEQ
 apt-get update && apt-get upgrade -y
-sudo apt-get install build-essential wget -y
+sudo apt-get install build-essential wget ufw -y
+ufw default deny incoming && ufw default allow outgoing
+ufw allow from any to any proto tcp port 22,443, 1194
+ufw enable
 echo "Bitte Link zu neuster Linux Server Version einfügen"
-echo "http://www.softether-download.com/files/softether/"
+echo "Von hier: http://www.softether-download.com/files/softether/"
 read link
 wget $link
 tar xzvf softether*
@@ -15,25 +16,25 @@ cd /usr/local/vpnserver/
 chmod 600 * 
 chmod 700 vpnserver 
 chmod 700 vpncmd
-wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/vpnserver?token=GHSAT0AAAAAABRB2ABR3GCQKETBTA66LYE6YQTJYJA
-mv vpnserver?token=GHSAT0AAAAAABRB2ABR3GCQKETBTA66LYE6YQTJYJA /etc/init.d/vpnserver
+wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/vpnserver?token=GHSAT0AAAAAABSKUF6PYHLXCYAOKFNVOZWIYR4YF6A
+mv vpnserver?token=GHSAT0AAAAAABSKUF6PYHLXCYAOKFNVOZWIYR4YF6A /etc/init.d/vpnserver
 mkdir /var/lock/subsys 
 chmod 755 /etc/init.d/vpnserver && /etc/init.d/vpnserver start 
 update-rc.d vpnserver defaults 
 ./vpncmd
-ServerPasswordSet 
-1
-HubCreate myhub 
-Hub myhub 
-SecureNatEnable 
-echo "Benutzername eingeben"
-read username
-UserCreate $username 
-UserPasswordSet $username 
-IPsecEnable
-yes
-ServerCertRegenerate 77.68.35.36
-ServerCertGet ~/cert.cer
-OpenVpnEnable yes /PORTS:1194
-OpenVpnMakeConfig ~/openvpn_config.zip
-ufw allow 500/udp && ufw allow 4500/udp && ufw allow 1194/udp
+# ServerPasswordSet 
+# 1
+# HubCreate myhub 
+# Hub myhub 
+# SecureNatEnable 
+# echo "Benutzername eingeben"
+# read username
+# UserCreate $username 
+# UserPasswordSet $username 
+# IPsecEnable
+# yes
+# ServerCertRegenerate 77.68.35.36
+# ServerCertGet ~/cert.cer
+# OpenVpnEnable yes /PORTS:1194
+# OpenVpnMakeConfig ~/openvpn_config.zip
+# ufw allow 500/udp && ufw allow 4500/udp && ufw allow 1194/udp
