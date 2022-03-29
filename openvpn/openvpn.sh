d0468f8ccc28946a46b9229855aceae015685b1a
manuel-its <98522752+manuel-its@users.noreply.github.com>
2022-03-30 01:18:12 +0200

    Update openvpn.sh

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -1,11 +1,6 @@
 ##!/bin/bash
-#link https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/openvpn.sh
-apt-get update && apt-get upgrade -y
-apt-get install build-essential gnupg2 gcc make wget ufw -y
-#ufw default deny incoming && ufw default allow outgoing
-#ufw allow from any to any proto tcp port 22,443, 1194
-#ufw enable
-cd home
+apt-get update -y
+apt-get install build-essential gnupg2 gcc make -y
 wget http://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 tar -xvzf softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 cd vpnserver
