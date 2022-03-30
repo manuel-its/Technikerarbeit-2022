57497a1e565e5c5f37cf1885dd461e64fbb6fe8d
manuel-its <98522752+manuel-its@users.noreply.github.com>
2022-03-30 16:31:44 +0200

    Update openvpn.sh

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -1,7 +1,7 @@
 ##!/bin/bash
 # https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/openvpn.sh
 apt-get update -y
-apt-get install build-essential gnupg2 gcc make -y
+apt-get install build-essential gnupg2 gcc expect make -y
 wget http://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/vpnserver
 mv vpnserver /etc/init.d/vpnserver
