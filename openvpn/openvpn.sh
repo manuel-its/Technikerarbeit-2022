c6dff79dc3dd7ba5bc776c5499b5e28fadfe70bb
manuel-its <98522752+manuel-its@users.noreply.github.com>
2022-03-30 21:48:05 +0200

    Update openvpn.sh

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -1,5 +1,5 @@
 ##!/bin/bash
-# https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/openvpn.sh
+# wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/openvpn/openvpn.sh && chmod +x openvpn.sh && bash openvpn.sh
 apt-get update -y
 apt-get install build-essential gnupg2 gcc expect make -y
 wget http://www.softether-download.com/files/softether/v4.38-9760-rtm-2021.08.17-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
@@ -18,8 +18,3 @@ chmod 755 /etc/init.d/vpnserver
 /etc/init.d/vpnserver start
 update-rc.d vpnserver defaults
 /usr/local/vpnserver/./vpncmd
-
-
-#autoexpect -f konfig.exp /usr/local/vpnserver/./vpncmd
-#expect script.exp ''
-# set serverpassword [lindex $argv 0]
