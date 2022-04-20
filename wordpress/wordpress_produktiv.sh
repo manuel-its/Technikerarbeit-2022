8bbcb84558e7f3edbe4aad80061fb33b5d9b857a
manuel-its <98522752+manuel-its@users.noreply.github.com>
2022-04-20 02:34:06 +0200

    0

--- wordpress/wordpress_produktiv.sh
+++ wordpress/wordpress_produktiv.sh
@@ -3,12 +3,17 @@ sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /et
 apt-key adv --fetch-keys 'https://packages.sury.org/php/apt.gpg' > /dev/null 2>&1
 sudo apt-get update && apt-get upgrade -y
 sudo apt-get install ca-certificates apt-transport-https software-properties-common php8.0 php8.0-mysql apache2 mariadb-server mariadb-client -y
-cd /var/www/
-sudo wget https://de.wordpress.org/latest-de_DE.tar.gz
-sudo tar xzvf latest-de_DE.tar.gz
-sudo mv /var/www/wordpress/ /var/www/html/
-sudo rm /var/www/latest-de_DE.tar.gz
-sudo rm /var/www/html/index.html
+wget https://raw.githubusercontent.com/manuel-its/Technikerarbeit-2022/main/wordpress/vpn-mig.com-multi.conf
+cp vpn-mig.com-multi.conf /etc/apache2/sites-available/
+rm /var/www/html/index.html
+# cd /var/www/
+# sudo wget https://de.wordpress.org/latest-de_DE.tar.gz
+# sudo tar xzvf latest-de_DE.tar.gz
+# sudo mv /var/www/wordpress/ /var/www/html/
+# sudo rm /var/www/latest-de_DE.tar.gz
+# sudo rm /var/www/html/index.html
+cpio -id < /var/www/wp.cpio
+mysqldump -u root wpdb < /var/www/wpdb.sql
 cd /var/www/html/wordpress/ && sudo cp wp-config-sample.php wp-config.php
 sudo chown -R www-data:www-data /var/www/html/wordpress
 echo "CREATE DATABASE wpdb;" | sudo mysql -u root
@@ -22,11 +27,14 @@ sudo sed -i "s/; short_open_tag/short_open_tag = On/" /etc/php/8.0/apache2/php.i
 sudo sed -i "s/memory_limit = 128M/memory_limit = 256M/" /etc/php/8.0/apache2/php.ini
 sudo sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 200M/" /etc/php/8.0/apache2/php.ini
 sudo sed -i "s/max_execution_time = 30/max_execution_time = 360/" /etc/php/8.0/apache2/php.ini
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
-sudo sed -i "s|/etc/ssl/certs/ssl-cert-snakeoil.pem|/etc/ssl/certs/apache-selfsigned.crt|" /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i "s|/etc/ssl/private/ssl-cert-snakeoil.key|/etc/ssl/private/apache-selfsigned.key|" /etc/apache2/sites-available/default-ssl.conf
+sudo certbot certonly --agree-tos --email info-vpn-mig@protonmail.com --webroot -w /var/www/html -d vpn-mig.com -d www.vpn-mig.com
+#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
+#sudo sed -i "s|/etc/ssl/certs/ssl-cert-snakeoil.pem|/etc/ssl/certs/apache-selfsigned.crt|" /etc/apache2/sites-available/default-ssl.conf
+#sudo sed -i "s|/etc/ssl/private/ssl-cert-snakeoil.key|/etc/ssl/private/apache-selfsigned.key|" /etc/apache2/sites-available/default-ssl.conf
 sudo a2enmod ssl
 sudo service apache2 restart
-sudo a2ensite default-ssl
+a2dissite 000-default.conf
+a2dissite default-ssl.conf
+sudo a2ensite vpn-mig.com-multi.conf
 sudo service apache2 restart
 
